31b4329c3e5d42f31311a407c543f8785b46279d
AnmolVerma7 <jamy1722@gmail.com>
2025-12-02 02:31:10 -0700

    fix: rebuild app with correct vite config

--- AnmolsPortfolio/vite.config.js
+++ AnmolsPortfolio/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // GitHub Pages base path
   plugins: [react()],
-  base: "/", // User Pages deploy to root domain
 });
